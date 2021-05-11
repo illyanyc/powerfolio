35bca5f0525e61789f4eabcc3a4bafe14af0d19c
Nathan S. Froemming <natfro@gmail.com>
2021-05-11 14:08:37 -0500

    Adding a helper function to calculate the logarithmic returns of an input dataframe

--- MyPortfolioSimulator.py
+++ MyPortfolioSimulator.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame
+import numpy as np
+from pandas import DataFrame, MultiIndex
 from typing import List
 
 
@@ -18,6 +19,35 @@ def get_attributes(df_alpaca: DataFrame, column_level: int = 1) -> List[str]:
     'low', 'close', 'volume', 'macd', 'rsi', 'sharpe_ratio', ...].
     """
     return df_alpaca.columns.get_level_values(column_level).unique().tolist()
+
+
+def get_log_returns(
+    df_alpaca: DataFrame,
+    input_label: str = 'close',
+    output_label: str = 'log_return',
+    diff_period: int = 1,
+) -> DataFrame:
+    """
+    Helper function to calculate the logarithmic returns of an input dataframe.
+    """
+    # Check that the user's input label is present in the dataframe
+    if not (input_label in get_attributes(df_alpaca)):
+        raise ValueError(f"Input label \"{input_label}\" not found in "
+                         f"dataframe column labels!")
+
+    # Cache shorthands (for brevity & clarity)
+    df = df_alpaca.loc[:, (slice(None), input_label)]
+    tickers = get_tickers(df)
+
+    # Calculate logarithmic returns
+    df_log_returns = np.log(df / df.shift(periods=diff_period))
+    df_log_returns.iloc[0, :] = 0.0  # replace leading `NaN` with correct value
+
+    # Set pandas multiindex
+    df_log_returns.columns = MultiIndex.from_product([tickers, [output_label]])
+
+    # Return the result
+    return df_log_returns
 
 
 class MyPortfolioSimulator:
