9b5793946774406a981e22f0b537c8ffc9c70eed
Nathan S. Froemming <natfro@gmail.com>
2021-05-13 12:44:16 -0500

    Creating an improved version of the efficient-frontier portfolio simulator

--- MyPortfolioSimulator.py
+++ MyPortfolioSimulator.py
@@ -111,89 +111,54 @@ class MyPortfolioSimulator:
 
     def __init__(
         self,
-        df_alpaca_ohclv: DataFrame,
+        df_alpaca: DataFrame,
+        keep_best_sharpe_ratios: int = 3,
         risk_free_rate: float = 0.0,
-        keep_best_sharpe_ratios: int = 5,
+        periods_per_annum: int = 252,
+        input_label: str = 'close',
         debug: bool = False,
     ):
-        # Get ticker symbols of the best financial instruments as measured by
-        # the Sharpe Ratio
-        sharpe_ratios = get_sharpe_ratios(df_alpaca_ohclv, risk_free_rate=risk_free_rate).squeeze().sort_values(ascending=True).dropna()
-        self.best_sharpe_ratios = sharpe_ratios[-keep_best_sharpe_ratios:]
-
-        # Get tickers of the best Sharpe Ratios
-        self.tickers = get_tickers(self.best_sharpe_ratios.to_frame().T)
-
-        # Calculate returns of the best financial instruments
-        df_returns = get_log_returns(df_alpaca_ohclv.loc[:, (self.tickers, 'close')])
-        df_returns.columns = self.tickers
-
-        # Cache expected return and covariance
-        self.df_returns_avg = df_returns.mean()
-        self.df_returns_cov = df_returns.cov()
-
-        # Cache other variables
+        # Assign member data
+        self.keep_best_sharpe_ratios = keep_best_sharpe_ratios
+        self.risk_free_rate = risk_free_rate
+        self.periods_per_annum = periods_per_annum
+        self.input_label = input_label
         self.debug = debug
+
+        # Get ticker symbols of financial instruments with the highest Sharpe Ratios
+        self.df_sharpe_ratios = get_sharpe_ratios(df_alpaca, risk_free_rate=risk_free_rate, periods_per_annum=periods_per_annum).squeeze().sort_values(ascending=True).dropna()[-keep_best_sharpe_ratios:]
+        self.tickers = get_tickers(self.df_sharpe_ratios.to_frame().T)
+
+        # Calculate expected returns and covariance
+        df_returns = get_log_returns(df_alpaca.loc[:, (self.tickers, self.input_label)])
+        df_returns.columns = self.tickers
+        self.df_returns_mean = df_returns.mean(axis=0)  # daily
+        self.df_returns_cov = df_returns.cov()
 
         # Inform user
         if self.debug:
-            print('Tickers')
-            print('-------')
-            print(self.tickers)
+            self.print()
+
+    def print(self) -> None:
+        """ Helper function to print member data. """
+        print('Tickers')
+        print('-------')
+        print(self.tickers)
+        print()
+        print('Annualized Sharpe Ratios')
+        print('------------------------')
+        print(self.df_sharpe_ratios)
+        print()
+        if 0:
+            print('Expected Mean Return')
+            print('--------------------')
+            print(self.df_returns_mean)
             print()
-            print('Best Sharpe Ratios')
-            print('------------------')
-            print(self.best_sharpe_ratios)
-            print()
-            print('Return Mean')
-            print('-----------')
-            print(self.df_returns_avg)
-            print()
-            print('Return Covariance')
-            print('-----------------')
+            print('Expected Return Covariance')
+            print('--------------------------')
             print(self.df_returns_cov)
             print()
-
-    def do_simulation(self, num_simulations: int = 500):
-        if self.debug:
-            print(f"`{self.do_simulation.__name__}()`")
-            print(self.tickers)
-            print()
-
-        # Cache variables
-        self.num_simulations = num_simulations
-        self.simulation_returns_vec = np.full(self.num_simulations, np.nan)
-        self.simulation_variances_vec = np.full(self.num_simulations, np.nan)
-        ntickers = len(self.tickers)
-
-        # Loop over num_simulations
-        for nsim in range(self.num_simulations):
-            # Generate random weights
-            weights = np.random.random(ntickers)
-
-            # Make sure the weight vector is normalized
-            weights /= np.sum(weights)
-
-            # Calculate expected portfolio return
-            #portfolio_return = np.dot(weights, self.df_returns_avg.to_numpy())
-            portfolio_return = weights @ self.df_returns_avg
-
-            # Calculate portfolio variance
-            portfolio_variance = weights @ self.df_returns_cov @ weights
-
-            # Add data to lists
-            self.simulation_returns_vec[nsim] = portfolio_return
-            self.simulation_variances_vec[nsim] = portfolio_variance
-
-            # Inform user
-            if (nsim == 0) or (((nsim + 1) % 100) == 0):
-                print(f"Simulation: ({nsim + 1} / {self.num_simulations})")
-                print(f"portfolio_return: {portfolio_return}")
-                print(f"portfolio_variance: {portfolio_variance}")
-                print()
-
-    def get_efficient_frontier(self):
-        pass
+        return None
 
 
 def test():
